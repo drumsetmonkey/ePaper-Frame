1c975e88c8403f90d055e4f7a4b36ec688149632
Andy <abelleisle@protonmail.com>
2021-12-04 02:03:47 -0500

    Added ESP32 configuration

---
+++ src/boards/ESP32/boarddefs.hpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+* File:             boarddefs.hpp
+*
+* Author:           Andy Belle-Isle (drumsetmonkey)  
+* Created:          12/04/21 
+* Description:      ESP32 Board Definitions
+*****************************************************************************/
+
+/**********************
+*  CONFIG NAMESPACE  *
+**********************/
+namespace config {
+
+    /******************
+    *  BOARD CONFIG  *
+    ******************/
+    namespace board {
+
+        /***************************
+        *  EPAPER PIN DEFINITIONS  *
+        ****************************/
+        namespace epaper {
+            const int CS   = 15;
+            const int DC   = 27;
+            const int RST  = 26;
+            const int BUSY = 25;
+        }
+
+        /*************************
+        *  SPI PIN DEFINITIONS  *
+        *************************/
+        namespace spi {
+            const int SCK  = 13;
+            const int MISO = 12;
+            const int MOSI = 14;
+            const int SS   = 15;
+        }
+    }
+}
+
